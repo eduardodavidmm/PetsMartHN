0a72af2e5189d7c1df654fe5640cc83207103e56
eduardodavidmm <eduardodavidmm@hotmail.com>
2024-04-27 14:11:02 -0600

    new landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,32 @@
 'use strict';
 
-
-
-/**
- * add event on element
- */
-
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
-  } else {
-    elem.addEventListener(type, callback);
-  }
-}
-
-
-
 /**
  * navbar toggle
  */
 
-const navToggler = document.querySelector("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
 const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 
-const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  navToggler.classList.toggle("active");
+const navElemArr = [overlay, navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
 }
-
-addEventOnElem(navToggler, "click", toggleNavbar);
-
-
-const closeNavbar = function () {
-  navbar.classList.remove("active");
-  navToggler.classList.remove("active");
-}
-
-addEventOnElem(navbarLinks, "click", closeNavbar);
 
 
 
 /**
- * active header when window scroll down to 100px
+ * add active class on header when scrolled 200px from top
  */
 
 const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-const activeElemOnScroll = function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-}
-
-addEventOnElem(window, "scroll", activeElemOnScroll);
+window.addEventListener("scroll", function () {
+  window.scrollY >= 200 ? header.classList.add("active")
+    : header.classList.remove("active");
+})
